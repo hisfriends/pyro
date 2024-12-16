da490eb363384c3e9f303e1a7cdd35f33ced3f48
塚田　翔 <tuka.k@me.com>
2024-12-16 17:07:39 +0900

    Add files via upload

---
+++ module/meta.php
@@ -0,0 +1,24 @@
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<meta name="keywords" content="sou's,福井県,南越前町" />
+<meta name="description" content="生活に寄り添いながら、家族のことを想い新しいエッセンスを創り出すこと。山々に包まれる今庄という地でロングライフに出会える場を提案いたします。" />
+<meta name="author" content="みつぐはうす工房" />
+<meta name="google-site-verification" content="1VeeN27gchi8SYooDOm-MD-aa_h7nfS4tpLWUMpWino" />
+<link rel="shortcut icon" href="../common/img/favicon.ico">
+<link rel="stylesheet" type="text/css" href="../vendor/fontawesome-free-5.0.6/web-fonts-with-css/css/fontawesome-all.css">
+<link rel="stylesheet" type="text/css" href="../common/css/normalize.css">
+<link rel="stylesheet" type="text/css" href="../common/css/fonts.css">
+<link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Audiowide&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../common/css/tag.css">
+<link rel="stylesheet" href="../common/css/header.css" />
+<link rel="stylesheet" href="../common/css/footer.css" />
+<link rel="stylesheet" href="../common/css/common.css" />
+<link rel="stylesheet" href="css/style.css" />
+<script src="../common/js/jquery-3.3.1.min.js"></script>
+<script src="../common/js/jquery.easings.min.js"></script>
+<script src="../common/js/header.js"></script>
+<script src="../common/js/common.js"></script>
+<script src="js/script.js"></script>
