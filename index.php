a8d9f227bfdf56920432ce2e0db3f0d31f9efb6c
塚田　翔 <tuka.k@me.com>
2024-12-17 11:04:22 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<title>PyroSparX | OFFICIAL WEB</title>
+<meta name="keywords" content="PyroSparX" />
+<meta name="description" content="PyroSparXのオフィシャル・ウェブサイトです。最新情報をお届けします。" />
+<meta name="author" content="PyroSparX" />
+<meta name="google-site-verification" content="1VeeN27gchi8SYooDOm-MD-aa_h7nfS4tpLWUMpWino" />
+<link rel="shortcut icon" href="common/img/favicon.ico">
+<link rel="stylesheet" type="text/css" href="vendor/fontawesome-free-5.0.6/web-fonts-with-css/css/fontawesome-all.css">
+<link rel="stylesheet" type="text/css" href="common/css/normalize.css">
+<link rel="stylesheet" type="text/css" href="common/css/fonts.css">
+<link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Audiowide&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="common/css/tag.css">
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="common/css/header.css" />
+<link rel="stylesheet" href="common/css/common.css" />
+<link rel="stylesheet" href="common/css/footer.css" />
+<script src="common/js/jquery-3.3.1.min.js"></script>
+<script src="common/js/jquery.easings.min.js"></script>
+<script src="common/js/header.js"></script>
+<script src="vendor/jquery.bgswitcher.js"></script>
+<script src="common/js/common.js"></script>
+<script src="js/script.js"></script>
+</head>
+<body>
+<div id="page_title" style="background-image:url('img/page_title.mp4')">
+  <video src="img/page_title.mp4" playsinline muted autoplay loop></video>
+  <a href="./">
+    <h2>
+      PyroSparX
+    </h2>
+  </a>
+  <h1>
+    <div class="img_wrapper">
+      <img src="common/img/logo_mark.jpg"/>
+    </div>
+  </h1>
+</div>
+<header>
+  <div id="header_fix">
+    <div class="header_inner">
+      <a href="./">
+        <div class="header_logo">
+        PyroSparX
+        </div>
+      </a>
+      <nav class="gnavi menu close">
+        <ul>
+          <li>
+            <a href="about"><span>about</span></a>
+          </li>
+          <li>
+            <a href="music"><span>music</span></a>
+          </li>
+          <li>
+            <a href="news"><span>news</span></a>
+          </li>
+          <li>
+            <a href="contact"><span>contact</span></a>
+          </li>
+        </ul>
+        <div class="sns">
+          <a href="https://twitter.com/TYGA_PSX">
+            <span>
+              <i class="fab fa-twitter"></i>
+            </span>
+          </a>
+          <a href="https://www.youtube.com/channel/UCWW-3c4Rq51UMH34IMKj4tw">
+            <span>
+              <i class="fab fa-youtube"></i>
+            </span>
+          </a>
+        </div>
+      </nav>
+    </div>
+    <button type="button" id="menuToggle">
+      <a id="menuButton">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </button>
+    <nav id="smartNav" class="close">
+      <ul>
+        <li>
+          <a href="about"><span>about</span></a>
+        </li>
+        <li>
+          <a href="music"><span>music</span></a>
+        </li>
+        <li>
+          <a href="news"><span>news</span></a>
+        </li>
+        <li>
+          <a href="contact"><span>contact</span></a>
+        </li>
+      </ul>
+      <div class="smartNavSns">
+        <p>FOLLOW US</p>
+        <a href="https://twitter.com/TYGA_PSX">
+          <i class="fab fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="https://www.youtube.com/channel/UCWW-3c4Rq51UMH34IMKj4tw">
+          <i class="fab fa-youtube" aria-hidden="true"></i>
+        </a>
+      </div>
+    </nav>
+  </div>
+</header>
+<div id="main_contents">
+  <section>
+    <div class="separate">
+      <div class="right">
+        <div class="script">
+          <h1>PyroSparX を詳しく知る</h1>
+          <p>
+            あらゆる楽曲のジャンルに関心を示す貪欲な創作意欲。<br>
+            それを追求するストイックな姿勢。<br>
+            クールなルックスに包まれた、鋭いアイロニーと溢れ出るパッション。<br>
+            独特な感性を持ち合わせていながらも、同世代の多くの人に支持されるスタイルをもっと詳しく知ろう
+          </p>
+          <div class="button">
+            <a href="about">About</a>
+          </div>
+        </div>
+      </div>
+      <div class="left">
+        <div class="img_wrapper">
+          <img src="img/about_01.jpg"/>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="back_img_title" style="background-image: url('img/music_01.jpg');">
+      <div class="inner">
+        <h1>
+          Music
+          <span>
+            オススメの楽曲
+          </span>
+        </h1>
+      </div>
+    </div>
+    <div class="music_list">
+      <div class="column">
+        <h1><span>Slider</span></h1>
+        <iframe width="560" height="450" src="https://www.youtube.com/embed/2nspdB95FjA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div class="column">
+        <h1><span>Sky Dome</span></h1>
+        <iframe width="560" height="450" src="https://www.youtube.com/embed/tsGNEqIJuEs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+
+      <div class="column">
+        <h1><span>NEXT STORY</span></h1>
+        <iframe width="560" height="450" src="https://www.youtube.com/embed/LQ15Kh4CDCk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+    <div class="button">
+			<a href="music">more music</a>
+		</div>
+    <div class="button">
+			<a href="https://www.youtube.com/channel/UCWW-3c4Rq51UMH34IMKj4tw/featured">Youtube</a>
+		</div>
+  </section>
+  <section>
+    <div class="back_img_title" style="background-image: url('img/news_01.jpg');">
+      <div class="inner">
+        <h1>
+          News
+          <span>
+            最新情報
+          </span>
+        </h1>
+      </div>
+    </div>
+    <div class="news_list">
+      <div class="column">
+        <span class="day">
+          2018.04.13.mon
+        </span>
+        <span class="title">
+          新曲、アップしました。
+        </span>
+      </div>
+      <div class="column">
+        <span class="day">
+          2018.04.11.sat
+        </span>
+        <span class="title">
+          WEBサイトをリニューアルいたしました。
+        </span>
+      </div>
+    </div>
+    <div class="button">
+			<a href="news">more news</a>
+		</div>
+  </section>
+  <section>
+    <div class="back_img_title" style="background-image: url('img/contact_01.jpg');">
+      <div class="inner">
+        <h1>
+          Contact
+          <span>
+            お問い合わせ
+          </span>
+        </h1>
+      </div>
+    </div>
+    <div class="separate">
+      <div class="left clear">
+        <div class="script">
+          <h1>楽曲の使用、お待ちしております</h1>
+          <p>
+            PyroSparX の楽曲は無料で使用できます。<br>
+            ご自身の活動・動画・イベントへの使用、商用利用も歓迎します。<br>
+            その際は、使用料は一切いただきませんが、私たちが使用状況を把握できるように、使用目的をご連絡ください。
+          </p>
+          <div class="button">
+            <a href="contact">Let's contact !</a>
+          </div>
+        </div>
+      </div>
+      <div class="right clear">
+        <div class="img_wrapper">
+          <img src="img/contact_02.jpg"/>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+<footer>
+	<div id="footer_top">
+		<div class="inner">
+			<div class="footer_01">
+        <div class="logo">
+          <div class="logo_img">
+            <a href="index.php" class="op">
+              <img src="common/img/logo_mark.jpg" alt="sou's">
+            </a>
+          </div>
+        </div>
+				<ul>
+          <li>
+            <a href="about"><span>about</span></a>
+          </li>
+          <li>
+            <a href="music"><span>music</span></a>
+          </li>
+          <li>
+            <a href="news"><span>news</span></a>
+          </li>
+          <li>
+            <a href="contact"><span>contact</span></a>
+          </li>
+				</ul>
+			</div>
+      <div class="footer_02">
+        <h3>Tokyo, Japan</h3>
+        <h4>COPYRIGHT 2019 PyroSparX ALL RIGHT RESERVED.</h4>
+    	</div>
+		</div>
+	</div>
+	<p id="page_top"><a href="#">PAGE TOP</a></p>
+</footer>
+</body>
+</html>
