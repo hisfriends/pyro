8f635dd09296a0282b1e541dc886558c9710d4b8
塚田　翔 <tuka.k@me.com>
2024-12-17 11:07:07 +0900

    Add files via upload

---
+++ news/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>news | PyroSparX</title>
+<?php
+	include('../module/meta.php');
+?>
+</head>
+<body>
+<div id="page_title">
+	<video src="img/page_title.mp4" playsinline muted autoplay loop></video>
+	<a href="../index.php">
+		<h2>
+    	PyroSparX
+  	</h2>
+	</a>
+	<h1>
+		<span class="big">News</span>
+		<span class="small">最新情報</span>
+	</h1>
+</div>
+<?php
+  include('../module/header.php');
+?>
+<div id="main_contents">
+	<section>
+		<div class="section_title">
+			Back number
+			<span>
+				バックナンバー
+			</span>
+		</div>
+		<div class="description">
+			<p>
+				PyroSparXの最新情報を掲載しております。なお、このページに掲載されている情報は、過去に掲載れたニュースも含むバックナンバーですので、中には終了しているイベントの情報も掲載されております。日時など、あらかじめご確認の上、ご覧ください。
+			</p>
+		</div>
+		<div class="news_list">
+			<div class="column">
+				<span class="day">
+					2018.04.13.mon
+				</span>
+				<span class="title">
+					新曲、アップしました。
+				</span>
+			</div>
+			<div class="column">
+				<span class="day">
+					2018.04.11.sat
+				</span>
+				<span class="title">
+					WEBサイトをリニューアルいたしました。
+				</span>
+			</div>
+		</div>
+		<div class="button">
+		  <a href="https://twitter.com/TYGA_PSX">Twitter を見る</a>
+		</div>
+  </section>
+</div>
+<?php
+  include('../module/footer.php');
+?>
+</body>
+</html>
