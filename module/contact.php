739d646179da52e16c00f5f1bddfaf6d881f2c9b
塚田　翔 <tuka.k@me.com>
2024-12-17 11:06:11 +0900

    Add files via upload

---
+++ module/contact.php
@@ -0,0 +1,17 @@
+<div class="contact" style="background-image: url('../img/contact_02.jpg');">
+  <div class="inner">
+    <div class="script">
+      <h1>
+        楽曲の使用、<br>お待ちしております
+      </h1>
+      <p>
+        PyroSparX の楽曲は無料で使用できます。
+        ご自身の活動・動画・イベントへの使用、商用利用も歓迎します。
+        その際は、使用料は一切いただきませんが、私たちが使用状況を把握できるように、使用目的をご連絡ください。
+      </p>
+      <div class="button">
+        <a href="../contact">Let's contact !</a>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ module/header.php
@@ -0,0 +1,71 @@
+<header>
+  <div id="header_fix">
+    <div class="header_inner">
+      <a href="../">
+    		<div class="header_logo">
+          PyroSparX
+        </div>
+      </a>
+      <nav class="gnavi menu close">
+        <ul>
+          <li>
+            <a href="../about"><span>about</span></a>
+          </li>
+          <li>
+            <a href="../music"><span>music</span></a>
+          </li>
+          <li>
+            <a href="../news"><span>news</span></a>
+          </li>
+          <li>
+            <a href="../contact"><span>contact</span></a>
+          </li>
+        </ul>
+        <div class="sns">
+          <a href="https://twitter.com/TYGA_PSX">
+            <span>
+              <i class="fab fa-twitter"></i>
+            </span>
+          </a>
+          <a href="https://www.youtube.com/channel/UCWW-3c4Rq51UMH34IMKj4tw">
+            <span>
+              <i class="fab fa-youtube"></i>
+            </span>
+          </a>
+        </div>
+      </nav>
+    </div>
+    <button type="button" id="menuToggle">
+      <a id="menuButton">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </button>
+    <nav id="smartNav" class="close">
+      <ul>
+        <li>
+          <a href="../about"><span>about</span></a>
+        </li>
+        <li>
+          <a href="../music"><span>music</span></a>
+        </li>
+        <li>
+          <a href="../news"><span>news</span></a>
+        </li>
+        <li>
+          <a href="../contact"><span>contact</span></a>
+        </li>
+      </ul>
+      <div class="smartNavSns">
+        <p>FOLLOW US</p>
+        <a href="https://twitter.com/TYGA_PSX">
+          <i class="fab fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="https://www.youtube.com/channel/UCWW-3c4Rq51UMH34IMKj4tw">
+          <i class="fab fa-youtube" aria-hidden="true"></i>
+        </a>
+      </div>
+    </nav>
+  </div>
+</header>
